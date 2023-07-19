0f17d230789d32d03809368337bd832bc800fcf5
raquelgm88 <raquel.rgm7@gmail.com>
2023-07-19 13:17:04 +0200

    Add history file in gitignore

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const machine = [piedra, papel, tijeras];
+const machine = ["piedra", "papel", "tijeras"];
 const option = document.querySelector('.js__input');
 const button = document.querySelector('.js__button');
 const msg = document.querySelector('.js__msg');
@@ -9,15 +9,16 @@ let randomNumber = 0;
 
 // Función para generar un número aleatorio entre 0 y 2
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 // Variable donde almaceno el número aleatorio
 randomNumber = getRandomNumber(max);
 
 // Función para pintar en la consola el número aleatorio y comprobar que funciona
-
 function renderNumber () {
-    console.log('El número aleatorio es el ' + randomNumber);
+  console.log('El número aleatorio es el ' + randomNumber);
 }
 renderNumber();
+
+//
