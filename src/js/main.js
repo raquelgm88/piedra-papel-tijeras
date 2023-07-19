0d8cc62318ed124caa983d3e9d44732b46541112
raquelgm88 <raquel.rgm7@gmail.com>
2023-07-19 13:41:08 +0200

    Create some functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const machine = ["piedra", "papel", "tijeras"];
+//VARIABLES
+const machine = ['piedra', 'papel', 'tijeras'];
 const option = document.querySelector('.js__input');
 const button = document.querySelector('.js__button');
 const msg = document.querySelector('.js__msg');
 const max = 3;
 let randomNumber = 0;
+
+
+// const options= {
+//     piedra: 0,
+//     papel: 1,
+//     tijeras: 2
+// }
+// const valorUsuario = document.querySelector('.js__input').toLowerCase();
+// const number = options[valorUsuario] // 1
+
+// FUNCIONES
 
 // Función para generar un número aleatorio entre 0 y 2
 function getRandomNumber(max) {
@@ -21,4 +33,21 @@ function renderNumber () {
 }
 renderNumber();
 
-//
+//Función para extraer el número que escribe el usuario
+function getOption () {
+  const userOption = parseInt(option.value);
+  return userOption;
+}
+
+//Función para comparar el número aleatorio con el del usuario
+function checkChoices () {
+    if 
+}
+
+function handleClickButton (event) {
+  event.preventDefault();
+}
+
+
+// EVENTOS
+button.addEventListener('click', handleClickButton);
