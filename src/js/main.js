46b34125fbd961a9266560b27b87ef06eb5dab55
raquelgm88 <raquel.rgm7@gmail.com>
2023-07-21 11:54:23 +0200

    Add new options to play

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,31 @@
 'use strict';
 
 //VARIABLES
-const machine = ['Piedra', 'Papel', 'Tijeras'];
+const machine = ['Piedra', 'Papel', 'Tijeras', 'Lagarto', 'Spock'];
 const rock = document.querySelector('.js__rock');
 const paper = document.querySelector('.js__paper');
 const scissors = document.querySelector('.js__scissors');
+const lizard = document.querySelector('.js__lizard');
+const spock = document.querySelector('.js__spock');
 const machineChoise = document.querySelector('.js__random');
 const msg = document.querySelector('.js__msg');
-const max = 3;
-
-// const options= {
-//     piedra: 0,
-//     papel: 1,
-//     tijeras: 2
-// }
-// const valorUsuario = document.querySelector('.js__input').toLowerCase();
-// const number = options[valorUsuario] // 1
-
+const max = 5;
+const options= {
+  '1_0': 1,
+  '2_0': 0,
+  '2_1': 2,
+  '3_0': 0,
+  '4_3': 3,
+  '4_2': 4,
+  '3_2': 2,
+  '3_1': 3,
+  '4_1': 1,
+  '4_0': 4
+};
 
 // FUNCIONES
 
-// Función para generar un número aleatorio entre 0 y 2
+// Función para generar un número aleatorio entre 0 y 4
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
 }
@@ -36,14 +41,21 @@ function renderMachineOption () {
 renderMachineOption();
 
 //Función para comparar el número aleatorio con el del usuario
-function checkChoices (id) {
-  if (id === randomNumber) {
-    msg.innerHTML = '¡Empate!';
-  } else if (randomNumber > id) {
-    randomNumber === 2 && id === 0 ? msg.innerHTML = '¡Has ganado!' : msg.innerHTML = '¡Has perdido!';
-  } else if(randomNumber < id && id) {
-    randomNumber === 0 && id === 2 ? msg.innerHTML = '¡Has perdido!' : msg.innerHTML = '¡Has ganado!';
-  }
+// function checkChoices (id) {
+//   if (id === randomNumber) {
+//     msg.innerHTML = '¡Empate!';
+//   } else if (randomNumber > id) {
+//     msg.innerHTML = randomNumber === 2 && id === 0 ? '¡Has ganado!' : '¡Has perdido!';
+//   } else if(randomNumber < id) {
+//     msg.innerHTML = randomNumber === 0 && id === 2 ? '¡Has perdido!' : '¡Has ganado!';
+//   }
+// }
+
+//Función anterior mejorada
+function checkChoices(id){
+  if(randomNumber === id) return '¡Empate!';
+  const key = randomNumber > id ? `${randomNumber}_${id}` : `${id}_${randomNumber}`;
+  return id === options[key] ? '¡Has ganado!' : 'Lo siento, has perdido';
 }
 
 //Función para pintar la opción de la máquina en pantalla
@@ -52,11 +64,11 @@ function showMachineOption () {
   machineChoise.innerHTML = choiceMsg + machine[randomNumber];
 }
 
-
+//Función del evento
 function handleClickButton (event) {
   event.preventDefault();
   const idBtn = parseInt(event.currentTarget.id);
-  checkChoices(idBtn);
+  msg.innerHTML = checkChoices(idBtn);
   showMachineOption();
 }
 
@@ -64,3 +76,5 @@ function handleClickButton (event) {
 rock.addEventListener('click', handleClickButton);
 paper.addEventListener('click', handleClickButton);
 scissors.addEventListener('click', handleClickButton);
+lizard.addEventListener('click', handleClickButton);
+spock.addEventListener('click', handleClickButton);
